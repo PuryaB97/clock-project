47b7b4517d7ed7b0c64c92056490b21d632aad36
Purya <65923099+PuryaB97@users.noreply.github.com>
2021-05-10 22:39:40 +0300

    main

---
+++ clockproject/clock.js
@@ -0,0 +1,48 @@
+function userName() {
+  var name = prompt("What's your name dear?");
+  document.getElementById('userName').innerText = name;
+}
+
+function showTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var day;
+
+  switch (new Date().getDay()) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+  }
+
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+
+  var time = h + ':' + m + ':' + s + ' ' + ' Today is ' + day;
+  document.getElementById('MyClockDisplay').innerText = time;
+  document.getElementById('MyClockDisplay').textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+userName();
+showTime();
